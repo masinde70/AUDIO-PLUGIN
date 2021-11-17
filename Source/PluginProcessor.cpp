d8495a64dfb1a74581ce0351c4fe24c46e958cd8
masinde70 <5812481+masinde70@users.noreply.github.com>
2021-11-17 12:57:17 +0200

    process context
    process context

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -157,6 +157,11 @@ void SimpleEQAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, juc
     auto leftBlock = block.getSingleChannelBlock(0);
     auto rightBlock = block.getSingleChannelBlock(1);
     
+    juce::dsp::ProcessContextReplacing<float> leftContext(leftBlock);
+    juce::dsp::ProcessContextReplacing<float> rightContext(rightBlock);
+    
+    leftChain.process(leftContext);
+    rightChain.process(rightContext);
     
 }
 
