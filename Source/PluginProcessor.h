899f40c367d0ae189125cd6298bbbe95534b196b
masinde70 <5812481+masinde70@users.noreply.github.com>
2021-11-23 10:19:08 +0200

    enum slope

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -9,6 +9,14 @@
 #pragma once
 
 #include <JuceHeader.h>
+
+enum Slope{
+    Slope_12,
+    Slope_24,
+    Slope_36,
+    Slope_48
+};
+
 struct ChainSettings{
     float peakFreq {0}, peakGainInDecibels{0}, peakQuality{1.f};
     float lowCutFreq {0}, highCutFreq {0};
